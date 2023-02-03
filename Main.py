ebaf4cc0b499e28823f644022caf167cc4c2fdf0
gitRisti <44567167+gitRisti@users.noreply.github.com>
2023-02-03 17:13:00 +0200

    UI update

--- Main.py
+++ Main.py
@@ -1,45 +1,174 @@
-#Results class for handling multiple results
-
-#EasyGUI for file explorer handling
 import easygui
+
 import os
 import csv
+import sys
+
+from random import randint
+
 from Result import Result
 
-import matplotlib.pyplot as plt
-from scipy.stats import linregress
+from Window import Ui_Form
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import QPen
+from PyQt5.QtWidgets import QListWidgetItem, QTableWidgetItem
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtGui import QColor
+from PyQt5.QtCore import Qt
 
-def retrieveFile():
-    files = easygui.fileopenbox(default = "*.itc",multiple=True)
-    return files
- 
-if __name__ == "__main__":
-    
-    results = []  
-    #Get .itc files
-    files = retrieveFile()
-    
-    for file in files:
-        
-        #Import ITC results text file and convert to list object
-        with open(file) as f:
-            content = csv.reader(f,delimiter=',')
-            contentList = list(content)
-        #basename -> get filename with extension
-        #splitext -> split filename and extension
-        result = Result(os.path.splitext(os.path.basename(file))[0],contentList)
-        results.append(result)
-    
+import xlsxwriter
+
+
+# Tab for showing raw data
+# Option to reduce number of injections plotted
+
+# Get data corresponding to selected items
+def select_data():
+    items = ui.listWidget.selectedItems()
+    result_list = []
+    for item in items:
+        match = item.text()
+        for result in results:
+            if match == result.name:
+                result_list.append(result)
+    return result_list
+
+
+def export_data():
+    summary_table = [['Name', 'Slope', 'R-squared']]
+    injection_export_table = [['Injection number', 'Injection length', 'Injection size', 'Concentration', 'Heat rate']]
+    for result in select_data():
+        summary_table.append([result.name, result.slope, result.rsquared])
+        injection_export_table.append([])
+    filename = easygui.filesavebox(msg='', title='Export data to Excel', default='', filetypes='.xlsx')
+    if filename != None:
+        workbook = xlsxwriter.Workbook(filename)
+
+        # Create Excel sheet that contains all exported fits
+        worksheet = workbook.add_worksheet('Summary')
+        row = 0
+        col = 0
+        for name, slope, rsquared in (summary_table):
+            if row > 0:
+                workbook.add_worksheet(name)
+            worksheet.write(row, col, name)
+            worksheet.write(row, col + 1, slope)
+            worksheet.write(row, col + 2, rsquared)
+            row += 1
+
+        workbook.close()
+
+
+def retrieve_file():
+    filenames = easygui.fileopenbox(default="*.itc", multiple=True)
+    return filenames
+
+
+# Plot graph by pulling data from a result object
+def draw_graphs(result):
+    # Scatter plot
+    ui.graphWidget.plot(result.x, result.y, pen=None, symbol='o', symbolBrush=result.color, symbolSize=10)
+    # Plot linear fit
+    pen = QPen(result.color, 0.02, Qt.SolidLine, Qt.RoundCap)
+    ui.graphWidget.plot(result.fit_x, result.fit_y, pen=pen)
+
+
+def check_match(name):
     for result in results:
-        plt.scatter(result.x,result.y)
-        fit = linregress(result.x,result.y)
-        plt.axline((0,0),slope=fit.slope)
-        plt.text(1,-0.2,f'R2: {fit.rvalue}\n slope: {fit.slope*4.184}')
-        plt.show()
-    
-    
+        if name == result.name:
+            return True
 
 
-    
+def draw_item():
+    ui.graphWidget.clear()
+    for result in select_data():
+        draw_graphs(result)
 
 
+def close_file():
+    ui.injection_table.clear()
+    ui.slope_label.clear()
+    ui.rsquared_label.clear()
+    ui.results_widget.clear()
+    ui.results_widget.setHorizontalHeaderLabels(['Conc.', 'Heat effect'])
+    items = ui.listWidget.selectedItems()
+    for item in items:
+        match = item.text()
+        for result in results:
+            if match == result.name:
+                ui.listWidget.takeItem(ui.listWidget.row(item))
+                results.remove(result)
+
+
+def update_data(result):
+    ui.injection_table.setText(str(result.injection_sizes) + "\n" + str(result.injection_spacings))
+    ui.slope_label.setText(f'Slope: {str(round(result.slope, 4))}')
+    ui.rsquared_label.setText(f'R-squared: {str(round(result.rsquared, 4))}')
+    ui.results_widget.setRowCount(len(result.injection_sizes) + 1)
+    for i, x in enumerate(result.x):
+        item = QTableWidgetItem(str(x))
+        ui.results_widget.setItem(i, 0, item)
+    for i, y in enumerate(result.y):
+        item = QTableWidgetItem(str(y))
+        ui.results_widget.setItem(i, 1, item)
+
+
+# def convert_heat():
+# Take syringe concentration and molecular weight, do conversion and return values to plot
+
+def display_data(item):
+    match = item.text()
+    for result in results:
+        if match == result.name:
+            update_data(result)
+
+
+def open_file():
+    # Get .itc files
+    files = retrieve_file()
+    if files != None:
+        for file in files:
+            # Import ITC results text file and convert to list object
+            with open(file) as f:
+                content = csv.reader(f, delimiter=',')
+                content_list = list(content)
+            # basename -> get filename with extension
+            # splitext -> split filename and extension
+            filename = os.path.splitext(os.path.basename(file))[0]
+
+            # Check if file is already open before continuing
+            match_boolean = check_match(filename)
+            if match_boolean:
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Critical)
+                msg.setText(f'{filename} is already open')
+                msg.exec_()
+                break
+            else:
+                result = Result(filename, content_list, ui.check_ignore.isChecked(),
+                                QColor(randint(25, 235), randint(25, 235), randint(25, 235), 80))
+                item = QListWidgetItem(result.name)
+                item.setBackground(result.color)
+                ui.listWidget.addItem(item)
+                results.append(result)
+                draw_graphs(result)
+                ui.listWidget.selectAll()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    ui.graphWidget.setBackground('w')
+    results = []
+    ui.open_button.clicked.connect(open_file)
+    ui.close_button.clicked.connect(close_file)
+    ui.export_button.clicked.connect(export_data)
+    ui.listWidget.itemSelectionChanged.connect(draw_item)
+    ui.listWidget.itemClicked.connect(display_data)
+    ui.results_widget.setColumnCount(2)
+    ui.results_widget.setHorizontalHeaderLabels(['Conc.', 'Heat effect'])
+
+    Form.show()
+    sys.exit(app.exec_())
--- Result.py
+++ Result.py
@@ -1,67 +1,92 @@
+from scipy.stats import linregress
+
+
 class Result:
-    
-    def stringToFloat(self,string):
-        splitString = string.split()
-        value = float(splitString[1]) 
+    # Class for converting a raw .itc file into a graphable result
+    @staticmethod
+    def string_to_float(string) -> float:
+        split_string = string.split()
+        value = float(split_string[1])
         return value
-    
-    def findListAverage(self,lst,start,end,index):
-        avg = sum(float(i[index]) for i in lst[start:end])/len(lst[start:end])
+
+    @staticmethod
+    def find_list_average(lst, start, end, index) -> float:
+        avg = sum(float(i[index]) for i in lst[start:end]) / len(lst[start:end])
         return avg
-    
-    def plotConcentration(self,sizes,concentration):
-        concList = [0]
-        for n in range(0,len(sizes)):
-            concList.append(sizes[n]*concentration+concList[n]) 
-        return concList
-    
-    #Handle calculation of average heat rates, data set and number of injections as input
-    def findHeatRate(self,data,n):
-        injectionLengths = []
-        injectionIndices = []
-        averageHeats = [0]
-        #Find injection locations in data
-        for i in range(1,n+1,1):
+
+    @staticmethod
+    def plot_concentration(sizes, concentration) -> list:
+        concentration_list = [0]
+        for n in range(0, len(sizes)):
+            concentration_list.append(sizes[n] * concentration + concentration_list[n])
+        return concentration_list
+
+    # Handle calculation of average heat rates, data set and number of injections as input
+    def find_heat_rate(self, data, n) -> list:
+        injection_indices = []
+        injection_lengths = []
+        average_heats = [0]
+        # Find injection locations in data
+        for i in range(1, n + 1, 1):
             for line in data:
                 if f'@{i}' in line[0]:
-                    injectionLengths.append(int(float(line[3])))
-                    injectionIndices.append(data.index(line)+1)
-        #Add virtual "last" injection
-        injectionIndices.append(len(data)+1)
-        #Calculate baseline
-        baseline = self.findListAverage(data,injectionIndices[1]-61,injectionIndices[1]-1,1)
-        #Calculate average heat rates of injections
-        for i in range(2,n+1,1):
-            averageHeats.append((self.findListAverage(data,injectionIndices[i]-61,injectionIndices[i]-1,1)) - baseline)
-        return averageHeats
-    
-    def findxy(self):
-        injectionSizes = []
-        numberOfInjections = 0
-        #Define beginning of injection table
+                    injection_lengths.append(int(float(line[3])))
+                    injection_indices.append(data.index(line) + 1)
+        # Add virtual "last" injection
+        injection_indices.append(len(data) + 1)
+        # Calculate baseline
+        self.baseline = self.find_list_average(data, injection_indices[1] - 61, injection_indices[1] - 1, 1) * 4.184
+        # Calculate average heat rates of injections
+        for i in range((2 if self.ignore_first else 1), n + 1, 1):
+            average_heats.append(
+                4.184 * (self.find_list_average(data, injection_indices[i] - 61, injection_indices[i] - 1, 1)) -
+                self.baseline)
+        return average_heats
+
+    def find_xy(self):
+        number_of_injections = 0
+        # Define beginning of injection table
         for line in self.data:
-          if 'ADC' in line[0]:
-              injectionIndex = self.data.index(line) + 2
-              
-        #Create arrays containing injection volumes and spacings         
-        for line in self.data[injectionIndex:]:
-            #Find end of injection table
+            if 'ADC' in line[0]:
+                injection_index = self.data.index(line) + 2
+
+        # Create arrays containing injection volumes and spacings
+        for line in self.data[injection_index:]:
+            # Find end of injection table
             if "# 0" in line[0]:
                 break
-            if numberOfInjections > 0:
-                injectionSizes.append(self.__stringToFloat(line[0]))
-            numberOfInjections += 1
-            
-        self.y = self.findHeatRate(self.data,numberOfInjections)
-        self.x = self.plotConcentration(injectionSizes,1)  
-        
-    def __init__(self,name,data):
+            if self.ignore_first:
+                if number_of_injections > 0:
+                    self.injection_sizes.append(self.string_to_float(line[0]))
+                    self.injection_spacings.append(int(line[2]))
+                number_of_injections += 1
+            else:
+                self.injection_sizes.append(self.string_to_float(line[0]))
+                self.injection_spacings.append(int(line[2]))
+                number_of_injections += 1
+        self.y = self.find_heat_rate(self.data, number_of_injections)
+        self.x = self.plot_concentration(self.injection_sizes, 1)
+
+        # Perform linear fit
+        fit = linregress(self.x, self.y)
+        self.fit_x = [0, self.x[-1]]
+        self.fit_y = [fit.intercept, self.fit_x[-1] * fit.slope + fit.intercept]
+        self.slope = fit[0]
+        self.rsquared = fit[2]
+
+    def __init__(self, name, data, ignore_boolean, color):
         self.name = name
         self.data = data
-        self.findxy()
-
-                  
-        
-    
-        
-        
+        self.color = color
+        self.ignore_first = ignore_boolean
+        self.injection_spacings = []
+        self.injection_sizes = []
+        self.baseline = 0
+        self.slope = 0
+        self.rsquared = 0
+        self.x = []
+        self.y = []
+        self.fit_x = []
+        self.fit_y = []
+        # Everything else has to be before find_xy()
+        self.find_xy()
